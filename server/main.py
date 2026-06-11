2818b9c06cc566d8cc5915ed41baf8a797703368
v-ishwa <m.viswa001@gmail.com>
2026-06-11 15:26:07 +0530

    feat: add web search service

--- server/main.py
+++ server/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from pydantic_models.chat_body import ChatBody
+from services.search_services import SearchService
 
 app = FastAPI()
 
+search_service = SearchService()
+
 @app.post("/chat")
 def chat_endpoint(body: ChatBody):
-    print(body.query)
-    return body.query
+    search_results = search_service.web_search(body.query)
+    print(search_results)
+    return search_results
  
